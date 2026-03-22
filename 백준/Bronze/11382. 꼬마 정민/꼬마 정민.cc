2e415f90a23d40cf7696d2c1e27d1c29940f459a
SeokHyeanIm <y_natge@naver.com>
2026-03-22 21:15:00 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+//꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+//
+//입력
+//첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+
+#include <iostream>
+
+int main()
+{
+    long long A, B, C;
+    std::cin >> A >> B >> C;
+    std::cout << A + B + C;
+    return 0;
+}
